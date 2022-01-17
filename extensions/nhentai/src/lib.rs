ef1fa01772aa2204e193bf739868dbdbd02e2a11
faldez <github@faldez.dev>
2022-01-17 07:51:13 +0700

    nhentai v0.1.5
    use old id because migrate back to scraping instead of api

--- extensions/nhentai/src/lib.rs
+++ extensions/nhentai/src/lib.rs
@@ -1,33 +1,22 @@
-mod dto;
-
-use std::collections::HashMap;
-
-use anyhow::bail;
-use dto::Result;
+use anyhow::{anyhow, Result};
+use chrono::NaiveDateTime;
+use fancy_regex::Regex;
+use scraper::{Html, Selector};
 use tanoshi_lib::prelude::{
     ChapterInfo, Extension, Input, InputType, Lang, MangaInfo, PluginRegistrar,
 };
 
 use lazy_static::lazy_static;
-use phf::phf_map;
 
-use crate::dto::Results;
-
-pub const ID: i64 = 9;
-pub const NAME: &str = "nhentai";
-pub const URL: &str = "https://nhentai.net";
+pub static ID: i64 = 6;
+pub static NAME: &str = "nhentai";
+pub static URL: &str = "https://nhentai.net";
 
 tanoshi_lib::export_plugin!(register);
 
 fn register(registrar: &mut dyn PluginRegistrar) {
     registrar.register_function(Box::new(NHentai::default()));
 }
-
-static IMAGE_TYPE: phf::Map<&'static str, &'static str> = phf_map! {
-    "j" => "jpg",
-    "g" => "gif",
-    "p" => "png",
-};
 
 lazy_static! {
     static ref TAG_FILTER: Input = Input::Text {
@@ -91,12 +80,14 @@ lazy_static! {
 }
 
 pub struct NHentai {
+    client: ureq::Agent,
     preferences: Vec<Input>,
 }
 
 impl Default for NHentai {
     fn default() -> Self {
         Self {
+            client: ureq::AgentBuilder::new().redirects(5).build(),
             preferences: PREFERENCES.clone(),
         }
     }
@@ -182,50 +173,57 @@ impl NHentai {
 
         query_str
     }
-}
 
-fn map_item_to_manga(item: &Result) -> MangaInfo {
-    let mut tags = HashMap::new();
-    for tag in item.tags.iter() {
-        let entry = tags
-            .entry(&tag.type_field)
-            .or_insert_with(|| vec![tag.name.clone()]);
-        entry.push(tag.name.clone());
-    }
+    fn get_manga_list(&self, url: &str) -> Result<Vec<MangaInfo>> {
+        let res = self.client.get(&url).call()?.into_string()?;
 
-    let mut description = format!("#{}", item.id);
-    if let Some(parody) = tags.get(&"parody".to_string()) {
-        description = format!("{}\nParodies: {}", description, parody.join(","))
-    }
-    if let Some(character) = tags.get(&"character".to_string()) {
-        description = format!("{}\nCharacters: {}", description, character.join(","))
-    }
-    if let Some(language) = tags.get(&"language".to_string()) {
-        description = format!("{}\nLanguage: {}", description, language.join(","))
-    }
-    if let Some(category) = tags.get(&"category".to_string()) {
-        description = format!("{}\nCategories: {}", description, category.join(","))
-    }
+        let document = Html::parse_document(&res);
+        let gallery_selector =
+            Selector::parse(".gallery").map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let thumbnail_selector =
+            Selector::parse("a > img").map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let path_selector =
+            Selector::parse("a").map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let title_selector = Selector::parse("a > .caption")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
 
-    MangaInfo {
-        source_id: ID,
-        title: item.title.pretty.clone(),
-        author: tags
-            .get(&"artist".to_string())
-            .cloned()
-            .unwrap_or_else(Vec::new),
-        genre: tags
-            .get(&"tag".to_string())
-            .cloned()
-            .unwrap_or_else(Vec::new),
-        status: None,
-        description: Some(description),
-        path: format!("/api/gallery/{}", item.id),
-        cover_url: format!(
-            "https://t.nhentai.net/galleries/{}/cover.{}",
-            item.media_id,
-            IMAGE_TYPE.get(&item.images.cover.t).unwrap()
-        ),
+        let mut manga_list = vec![];
+        for gallery in document.select(&gallery_selector) {
+            let cover_url = gallery
+                .select(&thumbnail_selector)
+                .flat_map(|thumbnail| thumbnail.value().attr("data-src"))
+                .next()
+                .map(|s| s.to_string())
+                .ok_or_else(|| anyhow!("cover_url not found"))?;
+
+            let path = gallery
+                .select(&path_selector)
+                .flat_map(|link| link.value().attr("href"))
+                .next()
+                .map(|s| s.to_string())
+                .ok_or_else(|| anyhow!("path not found"))?;
+
+            let title = gallery
+                .select(&title_selector)
+                .flat_map(|caption| caption.text().next())
+                .next()
+                .map(|s| s.to_string())
+                .ok_or_else(|| anyhow!("title not found"))?;
+
+            let manga = MangaInfo {
+                source_id: ID,
+                status: None,
+                title,
+                author: vec![],
+                genre: vec![],
+                description: None,
+                path,
+                cover_url,
+            };
+
+            manga_list.push(manga);
+        }
+        Ok(manga_list)
     }
 }
 
@@ -258,27 +256,11 @@ impl Extension for NHentai {
     }
 
     fn get_popular_manga(&self, page: i64) -> anyhow::Result<Vec<tanoshi_lib::prelude::MangaInfo>> {
-        let url = format!(
-            "{}/api/galleries/search?query={}&page={}&sort=popular",
-            URL,
-            self.query(None),
-            page
-        );
-
-        let res: Results = ureq::get(&url).call()?.into_json()?;
-        Ok(res.result.iter().map(map_item_to_manga).collect())
+        self.get_manga_list(&format!("{URL}/search/?q=\"\"&sort=popular&page={page}"))
     }
 
     fn get_latest_manga(&self, page: i64) -> anyhow::Result<Vec<tanoshi_lib::prelude::MangaInfo>> {
-        let url = format!(
-            "{}/api/galleries/search?query={}&page={}&sort=date",
-            URL,
-            self.query(None),
-            page
-        );
-
-        let res: Results = ureq::get(&url).call()?.into_json()?;
-        Ok(res.result.iter().map(map_item_to_manga).collect())
+        self.get_manga_list(&format!("{URL}/search/?q=\"\"&page={page}"))
     }
 
     fn search_manga(
@@ -288,64 +270,194 @@ impl Extension for NHentai {
         filters: Option<Vec<tanoshi_lib::prelude::Input>>,
     ) -> anyhow::Result<Vec<tanoshi_lib::prelude::MangaInfo>> {
         let url = if let Some(query) = query {
-            format!("{}/api/galleries/search?query={}&page={}", URL, query, page)
-        } else if let Some(filters) = filters {
-            format!(
-                "{}/api/galleries/search?query={}&page={}",
-                URL,
-                self.query(Some(filters)),
-                page
-            )
+            format!("{URL}/search/?q={query}&sort=popular&page={page}")
         } else {
-            bail!("query and filters can not be empty");
+            format!("{URL}/search/?q={}&page={page}", self.query(filters))
         };
 
-        println!("{url}");
-
-        let res: Results = ureq::get(&url).call()?.into_json()?;
-        Ok(res.result.iter().map(map_item_to_manga).collect())
+        self.get_manga_list(&url)
     }
 
     fn get_manga_detail(&self, path: String) -> anyhow::Result<tanoshi_lib::prelude::MangaInfo> {
-        let url = format!("{}{}", URL, path);
+        let url = format!("{}{}/", URL, path);
 
-        let res: dto::Result = ureq::get(&url).call()?.into_json()?;
-        Ok(map_item_to_manga(&res))
+        let res = self.client.get(&url).call()?.into_string()?;
+
+        let document = Html::parse_document(&res);
+        let gallery_id_selector = Selector::parse("h3[id=\"gallery_id\"]")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let parodies_selector = Selector::parse("a[href^=\"/parody/\"] > .name")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let characters_selector = Selector::parse("a[href^=\"/character/\"] > .name")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let languages_selector = Selector::parse("a[href^=\"/language/\"] > .name")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let categories_selector = Selector::parse("a[href^=\"/category/\"] > .name")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let pages_selector = Selector::parse("a[href^=\"/search/?q=pages\"] > .name")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let thumbnail_selector = Selector::parse("#cover > a > img")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let title_selector = Selector::parse("h1.title > .pretty")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let author_selector = Selector::parse("a[href^=\"/artist/\"] > .name")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let genre_selector = Selector::parse("a[href^=\"/tag/\"] > .name")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+
+        let mut description = "".to_string();
+        if let Some(gallery_id) = document.select(&gallery_id_selector).next().map(|el| {
+            el.text()
+                .into_iter()
+                .map(|id| id.to_string())
+                .collect::<Vec<String>>()
+                .join("")
+        }) {
+            description = format!("{}", gallery_id);
+        }
+        let parodies = document
+            .select(&parodies_selector)
+            .into_iter()
+            .filter_map(|el| el.text().next())
+            .collect::<Vec<&str>>()
+            .join(",");
+        if !parodies.is_empty() {
+            description = format!("{}\nParodies: {}", description, parodies);
+        }
+        let characters = document
+            .select(&characters_selector)
+            .into_iter()
+            .filter_map(|el| el.text().next())
+            .collect::<Vec<&str>>()
+            .join(",");
+        if !characters.is_empty() {
+            description = format!("{}\nCharacters: {}", description, characters);
+        }
+        let languages = document
+            .select(&languages_selector)
+            .into_iter()
+            .filter_map(|el| el.text().next())
+            .collect::<Vec<&str>>()
+            .join(",");
+        if !languages.is_empty() {
+            description = format!("{}\nLanguages: {}", description, languages);
+        }
+        let categories = document
+            .select(&categories_selector)
+            .into_iter()
+            .filter_map(|el| el.text().next())
+            .collect::<Vec<&str>>()
+            .join(",");
+        if !categories.is_empty() {
+            description = format!("{}\nCategories: {}", description, categories);
+        }
+        if let Some(pages) = document.select(&pages_selector).next().map(|el| {
+            el.text()
+                .into_iter()
+                .map(|id| id.to_string())
+                .collect::<Vec<String>>()
+                .join("")
+        }) {
+            description = format!("{}\nPages: {}", description, pages);
+        }
+
+        let cover_url = document
+            .select(&thumbnail_selector)
+            .flat_map(|el| el.value().attr("data-src"))
+            .next()
+            .map(|s| s.to_string())
+            .ok_or_else(|| anyhow!("cover not found"))?;
+
+        let title = document
+            .select(&title_selector)
+            .flat_map(|el| el.text())
+            .next()
+            .map(|s| s.to_string())
+            .ok_or_else(|| anyhow!("title not found"))?;
+
+        let author: Vec<String> = document
+            .select(&author_selector)
+            .flat_map(|el| el.text())
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
+
+        let genre: Vec<String> = document
+            .select(&genre_selector)
+            .flat_map(|el| el.text())
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
+
+        let manga = MangaInfo {
+            source_id: ID,
+            status: None,
+            path,
+            description: Some(description),
+            title,
+            author,
+            genre,
+            cover_url,
+        };
+
+        Ok(manga)
     }
 
     fn get_chapters(&self, path: String) -> anyhow::Result<Vec<tanoshi_lib::prelude::ChapterInfo>> {
-        let url = format!("{}{}", URL, path);
+        let url = format!("{}{}/", URL, path);
 
-        let res: dto::Result = ureq::get(&url).call()?.into_json()?;
-        Ok(vec![ChapterInfo {
+        let res = self.client.get(&url).call()?.into_string()?;
+        let document = Html::parse_document(&res);
+        let scanlator_selector = Selector::parse("a[href^=\"/group/\"] > .name")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let uploaded_selector = Selector::parse(".tags > time")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+        let scanlator = document
+            .select(&scanlator_selector)
+            .flat_map(|el| el.text())
+            .next()
+            .map(|s| s.to_string());
+        let uploaded = if let Some(uploaded) = document.select(&uploaded_selector).next() {
+            uploaded
+                .value()
+                .attr("datetime")
+                .and_then(|t| NaiveDateTime::parse_from_str(t, "%Y-%m-%dT%H:%M:%S%.f%z").ok())
+                .map(|dt| dt.timestamp())
+        } else {
+            None
+        };
+
+        let chapter = ChapterInfo {
             source_id: ID,
             title: "Chapter 1".to_string(),
             path,
-            number: 1.0,
-            scanlator: (!res.scanlator.is_empty()).then(|| res.scanlator),
-            uploaded: res.upload_date,
-        }])
+            number: 1_f64,
+            scanlator,
+            uploaded: uploaded.unwrap_or_else(|| 0),
+        };
+
+        Ok(vec![chapter])
     }
 
     fn get_pages(&self, path: String) -> anyhow::Result<Vec<String>> {
-        let url = format!("{}{}", URL, path);
+        let url = format!("{}{}/", URL, path);
 
-        let res: dto::Result = ureq::get(&url).call()?.into_json()?;
+        let res = self.client.get(&url).call()?.into_string()?;
 
-        Ok(res
-            .images
-            .pages
-            .iter()
-            .enumerate()
-            .map(|(i, p)| {
-                format!(
-                    "https://i.nhentai.net/galleries/{}/{}.{}",
-                    res.media_id,
-                    i + 1,
-                    IMAGE_TYPE.get(&p.t).unwrap()
-                )
-            })
-            .collect())
+        let document = Html::parse_document(&res);
+        let page_selector = Selector::parse(".thumb-container > .gallerythumb > img")
+            .map_err(|e| anyhow!("failed to parse selector: {e:?}"))?;
+
+        let mut pages = vec![];
+        for thumb in document.select(&page_selector) {
+            if let Some(url) = thumb.value().attr("data-src") {
+                let page = Regex::new(r#"(\d+)\/(\d+)t.(.+)$"#)
+                    .unwrap()
+                    .replace_all(url, "${1}/${2}.${3}")
+                    .to_string();
+                pages.push(page.replace("t.nhentai", "i.nhentai"));
+            }
+        }
+
+        Ok(pages)
     }
 
     fn headers(&self) -> std::collections::HashMap<String, String> {
@@ -370,6 +482,8 @@ mod test {
         let nhentai = NHentai::default();
         let res = nhentai.get_popular_manga(1).unwrap();
         assert!(!res.is_empty());
+        println!("{res:?}");
+        println!("-----------------")
     }
 
     #[test]
@@ -377,6 +491,8 @@ mod test {
         let nhentai = NHentai::default();
         let res = nhentai.get_latest_manga(1).unwrap();
         assert!(!res.is_empty());
+        println!("{res:?}");
+        println!("-----------------")
     }
 
     #[test]
@@ -386,6 +502,8 @@ mod test {
             .search_manga(1, Some("azur lane".to_string()), None)
             .unwrap();
         assert!(!res.is_empty());
+        println!("{res:?}");
+        println!("-----------------")
     }
 
     #[test]
@@ -409,23 +527,34 @@ mod test {
         }
         let res = nhentai.search_manga(1, None, Some(filters)).unwrap();
         assert!(!res.is_empty());
+        println!("{res:?}");
+        println!("-----------------")
+    }
+
+    #[test]
+    fn test_get_manga_detail() {
+        let nhentai = NHentai::default();
+        let res = nhentai.get_manga_detail("/g/385965".to_string());
+        assert!(res.is_ok());
+        println!("{res:?}");
+        println!("-----------------")
     }
 
     #[test]
     fn test_get_chapters() {
         let nhentai = NHentai::default();
-        let res = nhentai
-            .get_chapters("/api/gallery/385965".to_string())
-            .unwrap();
+        let res = nhentai.get_chapters("/g/385965".to_string()).unwrap();
         assert!(!res.is_empty());
+        println!("{res:?}");
+        println!("-----------------")
     }
 
     #[test]
     fn test_get_pages() {
         let nhentai = NHentai::default();
-        let res = nhentai
-            .get_pages("/api/gallery/385965".to_string())
-            .unwrap();
+        let res = nhentai.get_pages("/g/385965".to_string()).unwrap();
         assert!(!res.is_empty());
+        println!("{res:?}");
+        println!("-----------------")
     }
 }
